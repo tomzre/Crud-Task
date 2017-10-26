9326886f685e847909556be745bc130d2a32eb14
Tomasz Repel <tomaszrepel@yahoo.com>
2017-10-26 10:58:23 +0200

    Initilize Commit

---
+++ CapgeminiCrudTEST.WebApp/Controllers/CustomersController.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.Http;
+
+namespace CapgeminiCrudTEST.WebApp.Controllers
+{
+    public class CustomersController : ApiController
+    {
+    }
+}
---
+++ CapgeminiCrudTEST.WebApp/Startup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Owin;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Owin;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(CapgeminiCrudTEST.WebApp.Startup))]
+
+namespace CapgeminiCrudTEST.WebApp
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+
+            config.Formatters.JsonFormatter.MediaTypeMappings.Add(new QueryStringMapping("type", "json", new MediaTypeHeaderValue("application/json")));
+
+            var settings = config.Formatters.JsonFormatter.SerializerSettings;
+
+
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            settings.Formatting = Formatting.Indented;
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
